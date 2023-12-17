8904f2f50427000cf6210dfc094b0ef1482a8479
Pontus Sennerstam <sennerstam.pontus@gmail.com>
2023-12-17 12:31:23 +0100

    ✨ Add services buttons

---
+++ src/components/buttons/ServiceButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <a
+    :href="linkTo"
+    :class="[
+      'flex flex-col items-center gap-2 p-4 rounded-md transition-all duration-300',
+      `text-${textColor}`,
+    ]"
+  >
+    <div>
+      <component :is="icon" />
+    </div>
+    <p>{{ buttonText }}</p>
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'ServiceButton',
+  props: {
+    icon: {
+      type: Object,
+      default: () => {},
+    },
+    linkTo: {
+      type: String,
+      required: true,
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+    textColor: {
+      type: String,
+      default: 'white',
+    },
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,21 +4,44 @@
     <div id="white-space" class="h-24"></div>
   </div>
 
-  <div class="bg-primary-blue w-full min-h-[100px] my-5">
-    <div class="py-3 text-xl font-semibold text-center text-white">
+  <div class="bg-primary-blue w-full min-h-[100px] my-5 py-5">
+    <div class="py-6 text-xl font-semibold text-center text-white">
       <h2>Tjänster</h2>
     </div>
-    <div class="container"></div>
+    <div class="container flex justify-between">
+      <ServiceButton
+        class="hover:bg-primary-blue-dark"
+        :icon="CvIcon"
+        linkTo="/tjanster-cv"
+        buttonText="Få hjälp med CV"
+      />
+      <ServiceButton
+        class="hover:bg-primary-blue-dark"
+        :icon="InterviewIcon"
+        linkTo="/tjanster-intervju"
+        buttonText="Boka testintervju"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import HeroSection from '@/components/sections/HeroSection.vue';
+import ServiceButton from '@/components/buttons/ServiceButton.vue';
+import CvIcon from '@/_icons/CvIcon.vue';
+import InterviewIcon from '@/_icons/InterviewIcon.vue';
 import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'HomeView',
   components: {
     HeroSection,
+    ServiceButton,
+  },
+  data() {
+    return {
+      CvIcon,
+      InterviewIcon,
+    };
   },
 });
 </script>
