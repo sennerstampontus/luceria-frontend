23784469a36ec6b1bdb6145dff647d811167a501
postam <pontus.sennerstam@munters.com>
2024-02-19 22:58:13 +0100

    💄 Change the UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -33,12 +33,80 @@
   </div>
 
   <div class="hidden md:block md:w-full md:bg-luce-gray md:py-5">
-    <div class="logo-with-lines">CV granskning</div>
+    <div class="font-sans text-3xl font-semibold logo-with-lines">
+      Våra tjänster
+    </div>
 
-    <CVReviewView />
+    <div
+      :class="[
+        'container grid my-16 grid-columns section',
+        isVisable['RESUME'] ? 'fade-in' : '',
+      ]"
+    >
+      <div :class="'w-[150px]'">
+        <img
+          src="@/assets/resume-review.webp"
+          alt="resume.webp"
+          :class="['rounded-lg']"
+        />
+      </div>
+      <div ref="RESUME_REF">
+        <h2 class="pb-2 font-semibold text-center">CV Granskning</h2>
+        <p>
+          Är ditt CV anpassat för den position du ansöker till? Är det lättläst
+          och greppbart? Framhäver ditt CV dina styrkor på ett sätt som
+          engagerar rekryteraren? Genom att vända dig till oss
+          <strong>kan vi skapa ett elegant och professionellt CV.</strong>
+          Hjälper dig att ha tydlighet i ditt CV där det finns möjlighet det
+          skapar förvirring. Vi gör ditt CV mer överskådligt och
+          användarvänligt, vilket underlättar för rekryteraren att snabbt få en
+          klar bild av din kompetens.
+        </p>
+      </div>
+    </div>
+    <div
+      :class="[
+        'container grid my-16 grid-columns-rev section',
+        isVisable['INTERVIEW'] ? 'fade-in' : '',
+      ]"
+    >
+      <div ref="INTERVIEW_REF">
+        <h2 class="pb-2 font-semibold text-center">Test interview</h2>
+        <p>
+          Är du nervös inför din kommande intervju?
+          <strong>Boka en övningssession med oss</strong><br />
+          Vi kan assistera dig genom att bygga upp ditt självförtroende inför
+          intervjun. Det är ocksp viktigt att få konstruktiv feedback på vad du
+          kan förbättra inför din faktiska intervju, det hjäper vi dig med. Är
+          det mer du undrar eller funderar över? Vi finns här för att svara på
+          dina frågor.
+        </p>
+      </div>
+      <div class="w-[150px] justify-self-end">
+        <img
+          src="@/assets/test-interview.webp"
+          alt="testinterview.webp"
+          class="rounded-lg"
+        />
+      </div>
+    </div>
+
+    <!-- <CVReviewView />
   </div>
   <div class="hidden md:block md:py-5">
     <TestInterviewView />
+  </div> -->
+  </div>
+  <div
+    class="my-5 font-sans text-3xl font-semibold text-center logo-with-lines"
+  >
+    <h2>Om oss</h2>
+    <div
+      ref="ABOUT_REF"
+      :class="['section', isVisable['ABOUT'] ? 'fade-in' : '']"
+    >
+      TEST TEXT
+    </div>
   </div>
 
   <div class="h-[100px]"></div>
@@ -50,12 +118,14 @@ import ServiceCard from '@/components/cards/ServiceCard.vue';
 import ServiceButton from '@/components/buttons/ServiceButton.vue';
 import CvIcon from '@/_icons/CvIcon.vue';
 import InterviewIcon from '@/_icons/InterviewIcon.vue';
-import { computed, defineComponent, ref } from 'vue';
+import { computed, defineComponent, onMounted, onUnmounted, ref } from 'vue';
 import CVReviewView from './CVReviewView.vue';
 import TestInterviewView from './TestInterviewView.vue';
 export default defineComponent({
   name: 'HomeView',
   components: {
+    CvIcon,
+    InterviewIcon,
     HeroSection,
     ServiceCard,
     ServiceButton,
@@ -66,6 +136,32 @@ export default defineComponent({
   setup() {
     //REFS
     const onmouseover = ref(false);
+
+    const RESUME_REF = ref<HTMLElement | null>(null);
+    const INTERVIEW_REF = ref<HTMLElement | null>(null);
+    const ABOUT_REF = ref<HTMLElement | null>(null);
+
+    const isVisable = ref({
+      RESUME: false,
+      INTERVIEW: false,
+      ABOUT: false,
+    });
+
+    //METHODS
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          isVisable.value['RESUME'] = true;
+
+          setTimeout(() => {
+            isVisable.value['INTERVIEW'] = true;
+          }, 500);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.5 },
+    );
 
     //CONSTANTS
     const content = [
@@ -90,12 +186,28 @@ export default defineComponent({
       return state;
     });
 
+    //LIFE CYCLE HOOKS
+
+    onMounted(() => {
+      if (RESUME_REF.value) observer.observe(RESUME_REF.value);
+      if (INTERVIEW_REF.value) observer.observe(INTERVIEW_REF.value);
+      if (ABOUT_REF.value) observer.observe(ABOUT_REF.value);
+    });
+
+    onUnmounted(() => {
+      observer.disconnect();
+    });
+
     return {
       CvIcon,
       InterviewIcon,
       content,
       onmouseover,
       isHover,
+      RESUME_REF,
+      INTERVIEW_REF,
+      ABOUT_REF,
+      isVisable,
     };
   },
 });
@@ -127,5 +239,25 @@ export default defineComponent({
 
 .logo-with-lines::after {
   margin-left: 10px; /* Spacing between logo and line */
+}
+
+.grid-columns {
+  grid-template-columns: 1fr 2fr;
+  gap: 20px;
+  align-items: center;
+}
+.grid-columns-rev {
+  grid-template-columns: 2fr 1fr;
+  gap: 20px;
+  align-items: center;
+}
+
+.section {
+  opacity: 0;
+  transition: opacity 1s;
+}
+
+.section.fade-in {
+  opacity: 1;
 }
 </style>
